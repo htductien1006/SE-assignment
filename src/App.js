8e1695bba3873b6b680d3554b63ea88df8d7df56
Vo Duc Minh <92356136+minhVoDuc@users.noreply.github.com>
2022-12-05 14:54:02 +0700

    add vehicle-assign route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route }
     from 'react-router-dom';
 import SpecRoute from './pages/route-management/spec-route';
 import RouteList from './pages/route-management/route-list';
+import VehicleAssign from './pages/vehicle-assign/vehicle-assign';
 
 function App() {
   return (
@@ -12,9 +13,10 @@ function App() {
         <Routes>
           <Route exact path="/route-management/list"  element={<RouteList/>} />
           <Route exact path="/route-management/spec"  element={<SpecRoute/>} />
+          <Route exact path="/vehicle-assign"  element={<VehicleAssign/>} />
         </Routes>
       </BrowserRouter>
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -27,7 +29,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
