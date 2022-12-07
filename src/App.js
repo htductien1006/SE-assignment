9ce03fb534fc1274f9695a6d1f29778d4e2ba794
Hoang Nhat Ha <ha.hoang3788ha@hcmut.edu.vn>
2022-12-07 15:29:20 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,37 +1,27 @@
-// import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Routes, Route }
-    from 'react-router-dom';
-import SpecRoute from './pages/route-management/spec-route';
-import RouteList from './pages/route-management/route-list';
-import VehicleAssign from './pages/vehicle-assign/vehicle-assign';
-import RouteCreate from './pages/route-management/route-create';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SpecRoute from "./pages/route-management/spec-route";
+import RouteList from "./pages/route-management/route-list";
+import VehicleAssign from "./pages/vehicle-assign/vehicle-assign";
+import RouteCreate from "./pages/route-management/route-create";
+import Login from "./pages/login";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route exact path="/route-management/list"  element={<RouteList/>} />
-          <Route exact path="/route-management/spec"  element={<SpecRoute/>} />
-          <Route exact path="/route-management/create"  element={<RouteCreate/>} />
-          <Route exact path="/vehicle-assign"  element={<VehicleAssign/>} />
+          <Route path="/" element={<Login />} />
+          <Route exact path="/route-management/list" element={<RouteList />} />
+          <Route exact path="/route-management/spec" element={<SpecRoute />} />
+          <Route
+            exact
+            path="/route-management/create"
+            element={<RouteCreate />}
+          />
+          <Route exact path="/vehicle-assign" element={<VehicleAssign />} />
         </Routes>
       </BrowserRouter>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
