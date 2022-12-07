ee1aa32da915abf54c0f89945c01eba267528d01
Vo Duc Minh <92356136+minhVoDuc@users.noreply.github.com>
2022-12-07 08:10:45 +0700

    implement delete-route feature

--- src/pages/route-management/route-list.js
+++ src/pages/route-management/route-list.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UwcNavbar from "../uwc-navbar"
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 import "../../css/route-management.css";
 import iconTrash from "../../assets/page-management/iconTrash.svg"
 
-function deleteRoute() {
-  let text;
-  if (window.confirm("Press a button!") === true) {
-    text = "You pressed OK!";
-  } else {
-    text = "You canceled!";
-  }
-  window.alert(text);
-}
-
 const RouteList = () => {
+    const [routeList, setRoutes] = useState ([
+      {id: 1, status: 'Đang sử dụng', info: 'Độ dài: 2 Km, Đi qua: 3 MCP'},
+      {id: 2, status: 'Đang sử dụng', info: 'Độ dài: 2 Km, Đi qua: 3 MCP'},
+      {id: 3, status: 'Đang sử dụng', info: 'Độ dài: 2 Km, Đi qua: 3 MCP'},
+      {id: 4, status: 'Đang sử dụng', info: 'Độ dài: 2 Km, Đi qua: 3 MCP'},
+    ]);
+
+    const deleteRoute = (id) => {
+      const newRoutes = routeList.filter(
+        (route) => route.id !== id
+      );
+      setRoutes(newRoutes);
+    };
+
+    function confirmDelete(id) {
+      const MySwal = withReactContent(Swal)
+    
+      MySwal.fire({
+        title: 'Xóa route',
+        text: 'Bạn có muốn xóa route chứ?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Xóa',
+        cancelButtonText: 'Hủy'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          deleteRoute(id)
+        }
+      })
+    };
+
     return (
       <div className="route-management">
         <UwcNavbar/>
@@ -38,7 +63,16 @@ const RouteList = () => {
                 </tr>
               </thead>
               <tbody>
-                <tr className="table-row">
+                {routeList.map((route) => (
+                  <tr className="table-row" key={route.id}>
+                  <td className="table-cell cell-status">{route.status}</td>
+                  <td className="table-cell table-cell-info">
+                    <Link className="text-link" to={"../route-management/spec"}>{"Route " + route.id + ", " + route.info}</Link>
+                  </td> 
+                  <td className="table-cell" onClick={() => confirmDelete(route.id)}><img className="icon-trash-3" alt="trash-icon" src={iconTrash} /></td>
+                </tr>
+                ))}
+                {/* <tr className="table-row">
                   <td className="table-cell cell-status">Đang sử dụng</td>
                   <td className="table-cell table-cell-info">
                     <Link className="text-link" to={"../route-management/spec"}>Route 1 Độ dài: 2 Km Đi qua 3 MCP</Link>
@@ -65,7 +99,7 @@ const RouteList = () => {
                     <Link className="text-link" to={"../route-management/spec"}>Route 1 Độ dài: 2 Km Đi qua 3 MCP</Link>
                   </td>
                   <td className="table-cell" onClick={deleteRoute}><img className="icon-trash-3" alt="trash-icon" src={iconTrash}/></td>
-                </tr>
+                </tr> */}
               </tbody>
             </table>
           </div>
