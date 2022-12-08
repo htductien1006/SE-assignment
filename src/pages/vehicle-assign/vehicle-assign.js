20d27050885191189473d9788f440fc0c7cfb5ba
Vo Duc Minh <92356136+minhVoDuc@users.noreply.github.com>
2022-12-08 21:11:06 +0700

    implement feature choose vehicle

--- src/pages/vehicle-assign/vehicle-assign.js
+++ src/pages/vehicle-assign/vehicle-assign.js
@@ -1,26 +1,24 @@
 import React, {useState} from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import UwcNavbar from "../uwc-navbar/uwc-navbar"
 import "./vehicle-assign.css";
 
 const VehicleAssign = () => {
     const params = useParams();
-    const navigate = useNavigate();
-    var state_clicked = 'clicked', state_unclick = 'unclick';
     const truckList = [
-        {id: 0, state: state_unclick}, 
-        {id: 1, state: state_unclick},
-        {id: 2, state: state_unclick},
-        {id: 3, state: state_unclick}
+        {id: 0}, 
+        {id: 1},
+        {id: 2},
+        {id: 3}
     ];
     const trollerList = [
-        {id: 0, state: state_unclick}, 
-        {id: 1, state: state_unclick},
-        {id: 2, state: state_unclick},
-        {id: 3, state: state_unclick}
+        {id: 0}, 
+        {id: 1},
+        {id: 2},
+        {id: 3}
     ];
 
-    var [rows, setRows] = useState();
+    var rows;
 
     if (params.staffType === "janitor") { //janitor --> troller
         rows = trollerList.reduce(function (rows, key, index) { 
@@ -35,20 +33,7 @@ const VehicleAssign = () => {
         }, []);
     }
 
-    function handleOnClick(id) {
-        for (var i=0; i < rows.length; i++) {
-            var row = rows[i];
-            for (var j=0; j < row.length; j++) {
-                var col = row[j];
-                if (col.id === id) col.state = state_clicked;
-                else col.state = state_unclick;
-            }
-        }
-        const newRows = rows;
-        setRows(newRows);
-        console.log(newRows);        
-        navigate('/vehicle-assign/'+params.staffType, {replace: true});
-    }
+    const [currentID, setCurrentID] = useState();
 
     return (
       <div className="route-management">
@@ -68,61 +53,18 @@ const VehicleAssign = () => {
                         {rows.map((row) => (
                             <tr key={''+row[0].id+row[1].id} className="vehicle-table-row">
                                 {row.map((col) => (
-                                    <td key={col.id} className={`vehicle-table-data ${col.state === state_clicked ? 'table-data-clicked' : ''}`}
-                                        onClick={() => handleOnClick(col.id)}>
-                                        Vehicle ID #{col.id}
+                                    <td key={col.id} className="vehicle-table-data"
+                                        onChange={() => setCurrentID(col.id)}
+                                        style = {{backgroundColor: col.id === currentID && "rgb(200, 200, 200)"}}>
+                                        <input type="radio" id={col.id} name={params.staffType === "janitor" ? "TrollerList" : "TruckList"} value={col.id}/>
+                                        <label for={col.id}>Vehicle ID #{col.id}</label>
                                     </td>
                                 ))}
                             </tr>
                         ))}
-                        {/* <tr className="vehicle-table-row">
-                            <td className="vehicle-table-data">
-                                Vehicle ID #00
-                            </td>
-                            <td className="vehicle-table-data">
-                                Vehicle ID #01
-                            </td>
-                        </tr>
-                        <tr className="vehicle-table-row">
-                            <td className="vehicle-table-data">
-                                Vehicle ID #02
-                            </td>
-                            <td className="vehicle-table-data">
-                                Vehicle ID #03
-                            </td>
-                        </tr> */}
                     </tbody>
                 </table>
-            </div>
-
-            {/* <div className="vehicle-group">
-                <span className="vehicle-group-title">Truck</span>
-                <table className="vehicle-table">
-                    <colgroup>
-                      <col span="1" style={{width: 40 + '%'}} />
-                      <col span="1" style={{width: 40 + '%'}} />
-                    </colgroup>
-                    <tbody>
-                        <tr className="vehicle-table-row">
-                            <td className="vehicle-table-data">
-                                Vehicle ID #00
-                            </td>
-                            <td className="vehicle-table-data">
-                                Vehicle ID #01
-                            </td>
-                        </tr>
-                        <tr className="vehicle-table-row">
-                            <td className="vehicle-table-data">
-                                Vehicle ID #02
-                            </td>
-                            <td className="vehicle-table-data">
-                                Vehicle ID #03
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div> */}
-              
+            </div>              
             <div className="btn-submit">
                 <div className="btn-body">
                     <span className="btn-cancel">Cancel</span>
