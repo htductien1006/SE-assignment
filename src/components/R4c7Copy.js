e6da7697e3b2bdb156dd464552d5fc74a4e37947
Rosmarinus0301 <phu.tranphukt2002@hcmut.edu.vn>
2022-12-08 14:42:57 +0700

    janitor_calendar

---
+++ src/components/R4c7Copy.js
@@ -0,0 +1,18 @@
+import "./R4c7Copy.css";
+
+const R4c7Copy = ({ onClose }) => {
+  return (
+    <div className="r4c7-copy-div">
+      <div className="r4c7-content-div">
+        <span className="r4c7-content-txt-span">
+          <p className="vehicletroller-1-p5">Vehicle:troller 1</p>
+          <p className="vehicletroller-1-p5">Sáng: MCP 1, 2</p>
+          <p className="chiu-mcp-3">Chiều: MCP 3</p>
+        </span>
+      </div>
+      <div className="r4c7-text-div">25</div>
+    </div>
+  );
+};
+
+export default R4c7Copy;
