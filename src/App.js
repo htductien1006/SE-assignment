69d07de67587cce0ad6acf5acb11a6bff3490f59
htductien1006 <96772199+htductien1006@users.noreply.github.com>
2022-12-14 19:08:27 +0700

    ready for merge

--- src/App.js
+++ src/App.js
@@ -5,49 +5,52 @@ import {
   useLocation,
 } from "react-router-dom";
 import JanitorCalendar from "./pages/JanitorCalendar";
-import { useEffect } from "react";
+// import { useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  const action = useNavigationType();
-  const location = useLocation();
-  const pathname = location.pathname;
+  // const action = useNavigationType();
+  // const location = useLocation();
+  // const pathname = location.pathname;
 
-  useEffect(() => {
-    if (action !== "POP") {
-      window.scrollTo(0, 0);
-    }
-  }, [action]);
+  // useEffect(() => {
+  //   if (action !== "POP") {
+  //     window.scrollTo(0, 0);
+  //   }
+  // }, [action]);
 
-  useEffect(() => {
-    let title = "";
-    let metaDescription = "";
+  // useEffect(() => {
+  //   let title = "";
+  //   let metaDescription = "";
 
-    //TODO: Update meta titles and descriptions below
-    switch (pathname) {
-      case "/":
-        title = "";
-        metaDescription = "";
-        break;
-    }
+  //   //TODO: Update meta titles and descriptions below
+  //   switch (pathname) {
+  //     case "/":
+  //       title = "";
+  //       metaDescription = "";
+  //       break;
+  //   }
 
-    if (title) {
-      document.title = title;
-    }
+  //   if (title) {
+  //     document.title = title;
+  //   }
 
-    if (metaDescription) {
-      const metaDescriptionTag = document.querySelector(
-        'head > meta[name="description"]'
-      );
-      if (metaDescriptionTag) {
-        metaDescriptionTag.content = metaDescription;
-      }
-    }
-  }, [pathname]);
+  //   if (metaDescription) {
+  //     const metaDescriptionTag = document.querySelector(
+  //       'head > meta[name="description"]'
+  //     );
+  //     if (metaDescriptionTag) {
+  //       metaDescriptionTag.content = metaDescription;
+  //     }
+  //   }
+  // }, [pathname]);
 
   return (
-    <Routes>
-      <Route path="/" element={<JanitorCalendar />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<JanitorCalendar />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
