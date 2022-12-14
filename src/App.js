97df36312124677cfa55b1b8852a7282c21d22db
htductien1006 <96772199+htductien1006@users.noreply.github.com>
2022-12-14 18:01:36 +0700

    reformat

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 //import logo from './logo.svg';
 import './App.css';
-import { Routes, Route, Router } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import Layout from './Layouts/Layout';
 import AssignEmp from './pages/AssignMCPforEmployee/AssignMCPforJanitor'
 import InfoMCP from './pages/infoMCP/InfoMCP'
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<AssignEmp />} />  
-        <Route path="/info" element={<InfoMCP />} /> 
-      </Routes>
-    </Layout>
+    // <Layout>
+    //   <Routes>
+    //     <Route path="/" element={<AssignEmp />} />  
+    //     <Route path="/info" element={<InfoMCP />} /> 
+    //   </Routes>
+    // </Layout>
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AssignEmp />} /> 
+          <Route path="/info" element={<InfoMCP />} /> 
+        </Routes>
+      </BrowserRouter>
+    </div>  
   );
 }
 
