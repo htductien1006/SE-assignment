2a10e6198e6bdc215b855ada8f2356a3b3d93de0
htductien1006 <96772199+htductien1006@users.noreply.github.com>
2022-12-14 17:20:06 +0700

    reformat code for sync

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 //import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter} from "react-router-dom";
 import Layout from './Layouts/Layout';
 import Tabs from './components/BackOfficerDashboard/BackOfficerDashboard'
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Tabs />} />  
-      </Routes>
-    </Layout>
+    <div>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Tabs />} />  
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </div>
       
   );
 }
