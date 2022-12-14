1869fee07712b5348e9712f6cb7646c12085f644
htductien1006 <96772199+htductien1006@users.noreply.github.com>
2022-12-15 00:57:10 +0700

    tuan oke

--- src/pages/infoMCP/InfoMCP.js
+++ src/pages/infoMCP/InfoMCP.js
@@ -21,27 +21,27 @@ const InfoMCP = () => {
     <div>
       <UwcNavbar/>
       <div className={`tabs`}>
-        <ul className={`tabs__nav`}>
+        <ul className={`tabs_nav`}>
           <h1 className="text-center">PHÂN VIỆC JANITOR</h1>
         </ul>
-        <ul className={`tabs__return`}>
-          <h1 className="text-return"><Link to='/' ><button className="btn"><i className="gg-arrow-left"></i></button></Link>Thông tin MCP {assigned_MCP.id}</h1>
+        <ul className={`tabs_return`}>
+          <h1 className="text-return"><Link to='/AssignEmployee' ><button className="btn"><i className="gg-arrow-left"></i></button></Link>Thông tin MCP {assigned_MCP.id}</h1>
         </ul>
-        <div className={`tabs__content`} >
-          <div className={`tabs__content__1`}>
-            <div className={`tabs__content__1__Co1`}>
+        <div className={`tabs_content`} >
+          <div className={`tabs_content_1`}>
+            <div className={`tabs_content_1_Co1`}>
             <img src={require('./mcp1_image.jpg')} width = '90%'/>
               
             </div>
-            <div className={`tabs__content__1__Co2`}>
+            <div className={`tabs_content_1_Co2`}>
               <div>
                   <span className="textMCP-center" style = {{fontSize : "3.5rem"}}>Khu vực</span>
-                  <div style = {{color:"red", paddingTop: "1rem",fontSize : "2.2rem"}}>{assigned_MCP.address}</div>
+                  <div style = {{color:"red", paddingTop: "1rem",fontSize : "1.8rem"}}>{assigned_MCP.address}</div>
               </div>
               <div className ="capacity">
                   <span className="textMCP-center" style = {{fontSize : "3.5rem"}}>Dung tích</span>
-                  <div style = {{color:"black", paddingTop: "1rem",fontSize : "2.2rem"}}><span style = {{paddingRight: "15rem"}}>Hiện tại</span> {assigned_MCP.capacity.now}kg</div>
-                  <div style = {{color:"black", paddingTop: "1rem",fontSize : "2.2rem"}}><span style = {{paddingRight: "16.4rem"}}>Tối đa</span>  {assigned_MCP.capacity.max}kg</div>
+                  <div style = {{color:"black", paddingTop: "1rem",fontSize : "1.8rem"}}><span style = {{paddingRight: "15rem"}}>Hiện tại</span> {assigned_MCP.capacity.now}kg</div>
+                  <div style = {{color:"black", paddingTop: "1rem",fontSize : "1.8rem"}}><span style = {{paddingRight: "16.4rem"}}>Tối đa</span>  {assigned_MCP.capacity.max}kg</div>
               </div>
             </div>
           </div>
