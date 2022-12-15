108f7303a26518be9b7a6e0c45d89165d6defe94
htductien1006 <96772199+htductien1006@users.noreply.github.com>
2022-12-15 11:02:44 +0700

    ready to merge

--- src/component/edit-task/edit-task.js
+++ src/component/edit-task/edit-task.js
@@ -40,18 +40,18 @@ export const TaskInformation = () => {
 
     return (
         <div className="container">
-            <h1 className="text-center">TASK INFORMATION</h1>
+            <h1 className="text-center">TASK MANAGEMENT</h1>
             <div className="main-content p-4 row">
                 <div className="row mb-3">
                     <img src={INFO.avatar} alt='avatar' className="col-1 rounded-circle p-1"/>
-                    <div className="col-2 my-auto">
+                    <div className="col-1 my-auto">
                         <h1>{INFO.name}</h1>
                         <h5>{INFO.role}</h5>
                     </div>
-                    <h5 className="col-2 mt-5 pt-3">ID{INFO.id}</h5>
+                    <h5 className="col-1 mt-5 pt-3">ID{INFO.id}</h5>
                 </div>
-                <h4 className="text-center col-7" style={{ fontWeight: 'bold' }}>EDIT TASK</h4>
-                <div className="bg-light col-7 p-4 rounded-3 border border-dark">
+                <h4 className="text-center" style={{ fontWeight: 'bold' }}>EDIT TASK</h4>
+                <div className="bg-light p-4 rounded-3 border border-dark">
                     {tempo && tempo.task.map(todo => 
                         <div className='p-2'>
                             Buổi: 
@@ -59,11 +59,13 @@ export const TaskInformation = () => {
                                 value={todo.session} 
                                 onChange={handleSessionChange}
                                 className='col-6 ms-3 me-1'
+                                placeholder='Sáng/Chiều'
                             />
                             <input 
                                 value={todo.content} 
                                 onChange={handleContentChange}
                                 className='col-5'
+                                placeholder='Nội dung'
                             />
                         </div>
                     )}
@@ -85,7 +87,7 @@ export const TaskInformation = () => {
                     </button>
                     <div>
                         From <input type="date" className='col-5 me-5'/>
-                        to <input type="date" className='col-5'/>
+                        To <input type="date" className='col-5'/>
                     </div>
                     <div className="d-flex justify-content-center py-3">
                         <button 
@@ -102,12 +104,12 @@ export const TaskInformation = () => {
                         </button>
                     </div>
                 </div>
-                <div className='col-5 text-end py-5'>
+                {/* <div className='col-5 text-end py-5'>
                     <h4>Vehicle: Troller 1</h4>
                     <button className="btn btn-success mb-5">Assign vehicle</button>
                     <h4>MCP: M1, M2, M3</h4>
                     <button className="btn btn-danger">Assign MCP</button>
-                </div>
+                </div> */}
             </div>
         </div>
     );
