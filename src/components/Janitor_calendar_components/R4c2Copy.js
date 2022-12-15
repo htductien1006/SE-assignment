fd29f3b87196163c95098664b38ab1c2166b1ae6
Rosmarinus0301 <phu.tranphukt2002@hcmut.edu.vn>
2022-12-15 12:31:17 +0700

    gom components

---
+++ src/components/Janitor_calendar_components/R4c2Copy.js
@@ -0,0 +1,18 @@
+import "./R4c2Copy.css";
+
+const R4c2Copy = ({ onClose }) => {
+  return (
+    <div className="r4c2-copy-div">
+      <div className="r4c2-content-div">
+        <span className="r4c2-content-txt-span">
+          <p className="vehicletroller-1-p">Vehicle:troller 1</p>
+          <p className="vehicletroller-1-p">Sáng: MCP 1</p>
+          <p className="chiu-mcp-2">Chiều: MCP 2</p>
+        </span>
+      </div>
+      <div className="r4c2-text-div">20</div>
+    </div>
+  );
+};
+
+export default R4c2Copy;
