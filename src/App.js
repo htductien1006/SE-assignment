81fc8ad25d3486132107e022386263da40e49e64
htductien1006 <96772199+htductien1006@users.noreply.github.com>
2022-12-15 10:09:45 +0700

    ready to merge

--- src/App.js
+++ src/App.js
@@ -1,53 +1,58 @@
 import {
+  BrowserRouter,
   Routes,
   Route,
   useNavigationType,
   useLocation,
 } from "react-router-dom";
 import CollectorCalendar from "./pages/CollectorCalendar";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 function App() {
-  const action = useNavigationType();
-  const location = useLocation();
-  const pathname = location.pathname;
+  // const action = useNavigationType();
+  // const location = useLocation();
+  // const pathname = location.pathname;
 
-  useEffect(() => {
-    if (action !== "POP") {
-      window.scrollTo(0, 0);
-    }
-  }, [action]);
+  // useEffect(() => {
+  //   if (action !== "POP") {
+  //     window.scrollTo(0, 0);
+  //   }
+  // }, [action]);
 
-  useEffect(() => {
-    let title = "";
-    let metaDescription = "";
+  // useEffect(() => {
+  //   let title = "";
+  //   let metaDescription = "";
 
-    //TODO: Update meta titles and descriptions below
-    switch (pathname) {
-      case "/":
-        title = "";
-        metaDescription = "";
-        break;
-    }
+  //   //TODO: Update meta titles and descriptions below
+  //   switch (pathname) {
+  //     case "/":
+  //       title = "";
+  //       metaDescription = "";
+  //       break;
+  //   }
 
-    if (title) {
-      document.title = title;
-    }
+  //   if (title) {
+  //     document.title = title;
+  //   }
 
-    if (metaDescription) {
-      const metaDescriptionTag = document.querySelector(
-        'head > meta[name="description"]'
-      );
-      if (metaDescriptionTag) {
-        metaDescriptionTag.content = metaDescription;
-      }
-    }
-  }, [pathname]);
+  //   if (metaDescription) {
+  //     const metaDescriptionTag = document.querySelector(
+  //       'head > meta[name="description"]'
+  //     );
+  //     if (metaDescriptionTag) {
+  //       metaDescriptionTag.content = metaDescription;
+  //     }
+  //   }
+  // }, [pathname]);
 
   return (
-    <Routes>
-      <Route path="/" element={<CollectorCalendar />} />
-    </Routes>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/CollectorCalendar" element={<CollectorCalendar/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 export default App;
