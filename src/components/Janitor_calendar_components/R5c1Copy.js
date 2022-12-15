007b4c2cacf0bc11e574273e30900ca65c58c112
Rosmarinus0301 <phu.tranphukt2002@hcmut.edu.vn>
2022-12-15 12:31:17 +0700

    gom components

---
+++ src/components/Janitor_calendar_components/R5c1Copy.js
@@ -0,0 +1,18 @@
+import "./R5c1Copy.css";
+
+const R5c1Copy = ({ onClose }) => {
+  return (
+    <div className="r5c1-copy-div">
+      <div className="r5c1-content-div">
+        <span className="r5c1-content-txt-span">
+          <p className="vehicletroller-1-p6">Vehicle:troller 1</p>
+          <p className="vehicletroller-1-p6">Sáng: MCP 1, 2</p>
+          <p className="chiu-mcp-31">Chiều: MCP 3</p>
+        </span>
+      </div>
+      <div className="r5c1-text-div">26</div>
+    </div>
+  );
+};
+
+export default R5c1Copy;
